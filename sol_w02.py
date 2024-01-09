392381ec0c1a65dd9e9cbef893d6171a8f10a6a6
HomayounJafari <75078042+homayounjafari@users.noreply.github.com>
2024-01-09 13:51:26 +0100

    Add files via upload

---
+++ sol_w02.py
@@ -0,0 +1,23 @@
+OK_FORMAT = True
+test = {
+  'name': 'Question',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> sum
+          -0.5
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
